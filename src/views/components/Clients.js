00f1c1acd88839ea2af59d2659530ff7f6505030
Digital-Banjare <digitalbanjare@gmail.com>
2025-01-27 22:09:16 -0500

    Update Clients.js

--- src/views/components/Clients.js
+++ src/views/components/Clients.js
@@ -189,166 +189,125 @@ const Clients = () => {
             <hr className="my-4" />
             <Card className="bg-secondary shadow">
               <CardHeader className="bg-white border-0">
-                <Row className="align-items-center">
-                  <Col xs="8">
-                    <h3 className="mb-0">Invoice #123453</h3>
-                  </Col>
-                  <Col className="text-right" xs="4">
-                    <Button
-                      color="primary"
-                      href="#"
-                      onClick={(e) => e.preventDefault()}
-                      size="sm"
-                    >
-                      Print
-                    </Button>
-                  </Col>
-                </Row>
+                <h3 className="mb-0">Client List</h3>
               </CardHeader>
-              <CardBody>
-                  <div>
-                    <Row className="my-4 d-flex">
-                      <Col lg="6">
-                        <div>
-                          <div className="form-control-label">
-                            VoxLeads
-                          </div>
-                          <div>
-                            <p>Embankment, 050105 Bucharest, Romania</p>
-                            <p>tel: +4 (074) 1090873</p>
-                          </div>
-                        </div>
-                      </Col>
-                      <Col lg="6" className="text-right">
-                        <div>
-                          <div className="form-control-label">
-                            Billed to: John Doe
-                          </div>
-                          <div>
-                            <p>4006 Locust View Drive</p>
-                            <p>San Francisco CA</p>
-                            <p>California</p>
-                          </div>
-                        </div>
-                      </Col>
-                    </Row>
-                    <Row className="my-4">
-                      <Col lg="6">
-                        <div>
-                          <div className="form-control-label">
-                            Invoice #
-                          </div>
-                          <div>
-                            123453
-                          </div>
-                        </div>
-                      </Col>
-                      <Col lg="6">
-                        <div>
-                          <div className="form-control-label">
-                            Invoice Date
-                          </div>
-                          <div>
-                            2025-01-27
-                          </div>
-                        </div>
-                      </Col>
-                    </Row>
-                  </div>
-                  <hr className="my-4" />
-                  <h6 className="heading-small text-muted mb-4">
-                    Invoice Summary
-                  </h6>
-                  <Table className="align-items-center table-flush" responsive>
-                    <thead className="thead-light">
-                      <tr>
-                        <th scope="col">Item</th>
-                        <th scope="col">Qty</th>
-                        <th scope="col">Rate</th>
-                        <th scope="col">Amount</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">
-                        <Media className="align-items-center">
-                            <span className="mb-0 text-sm">
-                            Agent minutes
-                            </span>
-                        </Media>
-                        </th>
-                        <td>4000</td>         
-                        <td>
-                        <div className="invoice-qty">
-                            $0.09 / m
-                        </div>
-                        </td>
-                        <td>
-                        <div className="invoice-amount">
-                            $360
-                        </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-                        <Media className="align-items-center">
-                            <span className="mb-0 text-sm">
-                            Agent (3 pack)
-                            </span>
-                        </Media>
-                        </th>
-                        <td>1</td>         
-                        <td>
-                        <div className="invoice-qty">
-                            $11.99 / mo
-                        </div>
-                        </td>
-                        <td>
-                        <div className="invoice-amount">
-                            $11.99
-                        </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-                        <Media className="align-items-center">
-                            <span className="mb-0 text-sm">
-                            Agent (single)
-                            </span>
-                        </Media>
-                        </th>
-                        <td>1</td>         
-                        <td>
-                        <div className="invoice-qty">
-                            $5.99 / mo
-                        </div>
-                        </td>
-                        <td>
-                        <div className="invoice-amount">
-                            $5.99
-                        </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-                         &nbsp;
-                        </th>
-                        <td>&nbsp;</td>         
-                        <td>
-                        <div className="text-xl font-weight-bold">
-                            Total
-                        </div>
-                        </td>
-                        <td>
-                        <div className="text-xl font-weight-bold">
-                            $377.98
-                        </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                  </Table>
-                  <hr className="my-4" />
-              </CardBody>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Client ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone Numer</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      <Media className="align-items-center">
+                        <span className="mb-0 text-sm">
+                          123
+                        </span>
+                      </Media>
+                    </th>
+                    <td>
+                      <div>
+                        Client Name
+                      </div>
+                    </td>         
+                    <td>
+                      <div>
+                        n/a
+                      </div>
+                    </td>
+                    <td>
+                      <div>
+                        (333) 765-4321
+                      </div>
+                    </td>
+                    <td className="text-center">
+                      <UncontrolledDropdown>
+                        <DropdownToggle
+                          className="btn-icon-only"
+                          href="#pablo"
+                          role="button"
+                          size="sm"
+                          color=""
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          <i className="fas fa-angle-down" />
+                        </DropdownToggle>
+                        <DropdownMenu className="dropdown-menu-arrow" right>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            View Client
+                          </DropdownItem>
+                          <DropdownItem
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            Delete Client
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="disabled">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
             </Card>
           </Col>
         </Row>
