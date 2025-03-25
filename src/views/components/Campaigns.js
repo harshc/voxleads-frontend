f32c529c40efba954c8ec3c1e9e38972bbb7edaa
Digital-Banjare <digitalbanjare@gmail.com>
2025-03-25 13:26:22 -0500

    Update Campaigns.js
    fixed errors

--- src/views/components/Campaigns.js
+++ src/views/components/Campaigns.js
@@ -804,19 +804,19 @@ const Campaigns = () => {
                       <Row>
                         <Col lg="12">
                           <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <Input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
                             <label className="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
                             </div>
                             <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+                            <Input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
                             <label className="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
                             </div>
                             <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDisabled" disabled>
+                            <Input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDisabled" disabled />
                             <label className="form-check-label" for="flexSwitchCheckDisabled">Disabled switch checkbox input</label>
                             </div>
                             <div class="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled" checked disabled>
+                            <Input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled" checked disabled />
                             <label className="form-check-label" for="flexSwitchCheckCheckedDisabled">Disabled checked switch checkbox input</label>
                           </div>
                         </Col>
